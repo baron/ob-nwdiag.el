b110ed0baeb2c49472106526f8810def564e518c
Sam Baron <richstyles@gmail.com>
2019-02-04 15:18:12 +0900

    rename blockdiag to nwdiag

--- ob-nwdiag.el
+++ ob-nwdiag.el
@@ -18,43 +18,43 @@
 ;; Author: Dmitry Moskowski
 ;; Keywords: tools, convenience
 ;; Package-Version: 20170501.112
-;; Homepage: https://github.com/corpix/ob-blockdiag.el
+;; Homepage: https://github.com/baron/ob-nwdiag.el
 
 ;; This file is NOT part of GNU Emacs.
 
 ;;; Commentary:
 
-;; Org-Babel support for evaluating blockdiag source code.
+;; Org-Babel support for evaluating nwdiag source code.
 
 ;;; Requirements:
 
-;; - blockdiag :: http://blockdiag.com/en/
+;; - nwdiag :: http://nwdiag.com/en/
 
 ;;; Code:
 (require 'ob)
 
-(defvar org-babel-default-header-args:blockdiag
+(defvar org-babel-default-header-args:nwdiag
   '(
     (:results . "file")
     (:exports . "results")
-    (:tool    . "blockdiag")
+    (:tool    . "nwdiag")
     (:font    . nil)
     (:size    . nil))
-  "Default arguments for drawing a blockdiag image.")
+  "Default arguments for drawing a nwdiag image.")
 
-(add-to-list 'org-src-lang-modes '("blockdiag" . blockdiag))
+(add-to-list 'org-src-lang-modes '("nwdiag" . nwdiag))
 
-(defun org-babel-execute:blockdiag (body params)
+(defun org-babel-execute:nwdiag (body params)
   (let ((file (cdr (assoc :file params)))
         (tool (cdr (assoc :tool params)))
         (font (cdr (assoc :font params)))
         (size (cdr (assoc :size params)))
 
-        (buffer-name "*ob-blockdiag*")
+        (buffer-name "*ob-nwdiag*")
         (error-template "Subprocess '%s' exited with code '%d', see output in '%s' buffer"))
     (save-window-excursion
       (let ((buffer (get-buffer buffer-name)))(if buffer (kill-buffer buffer-name) nil))
-      (let ((data-file (org-babel-temp-file "blockdiag-input"))
+      (let ((data-file (org-babel-temp-file "nwdiag-input"))
             (args (append (list "-o" file)
                           (if size (list "--size" size) (list))
                           (if font (list "--font" font) (list))))
@@ -64,5 +64,5 @@
             ((exit-code (apply 'call-process tool nil buffer nil (append args (list data-file)))))
           (if (= 0 exit-code) nil (error (format error-template tool exit-code buffer-name))))))))
 
-(provide 'ob-blockdiag)
-;;; ob-blockdiag.el ends here
+(provide 'ob-nwdiag)
+;;; ob-nwdiag.el ends here
